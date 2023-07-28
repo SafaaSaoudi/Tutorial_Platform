2fcba855723960c975eb22ecee0391614abf304d
Eya Somai <eya.somai@esprit.tn>
2023-07-28 19:41:51 +0100

    squellette

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,11 @@
-module.exports={
-    mongo : {
-        uri:"mongodb+srv://eyasomai:0000@tutoapp.ipta4hq.mongodb.net/"
+const mongoose = require ("mongoose")
+const config=async()=>{
+    try { 
+        await mongoose.connect("mongodb+srv://eyasomai:0000@tutoapp.ipta4hq.mongodb.net/")
+        console.log("connetion reussi")
+        
+    } catch (error) {
+        console.log("erreur de connexionn")
     }
 }
+module.exports=config
