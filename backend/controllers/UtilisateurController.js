b387644d3105f43a4d403e2bb2b2c60b9e189f82
Eya Somai <eya.somai@esprit.tn>
2023-07-30 02:24:00 +0100

    base

--- backend/controllers/UtilisateurController.js
+++ backend/controllers/UtilisateurController.js
@@ -1,4 +1,4 @@
-const Users = require( "../models/Utilisateur" );
+const Users = require( "../models/user" );
 const bcrypt = require("bcrypt");
 exports.AjouterUtilisateur = async ( req, res ) =>
 {
