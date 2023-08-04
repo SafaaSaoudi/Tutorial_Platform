d33e9b0990459bd70337f982ad5b8da2738719b4
mkh-dev <malek.khelil@esprit.tn>
2023-08-04 12:07:08 +0100

    TP-2 Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,15 @@
-const mongoose=require("mongoose")
-const userSchema=new mongoose.Schema({
-    name:{type:String,required: true},
-    lastName:{type:String,required: true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    image:{type:String,required :false},
-    role:{type:String,enum:["user","admin"],default:"user"}
-})
-module.exports=mongoose.model("users",userSchema)    
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordCode: { type: String, required: false }, 
+  image: { type: String, required: false },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model("users", userSchema);
