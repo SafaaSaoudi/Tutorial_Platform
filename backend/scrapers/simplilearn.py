a6af13ab6e16fb432ba6ddec49ff5a03513dd2cb
mkh-dev <malek.khelil@esprit.tn>
2023-08-26 14:52:15 +0100

    TP-8 Datacleaning simplilearn and udemy

--- backend/scrapers/simplilearn.py
+++ backend/scrapers/simplilearn.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+import re
 
 client = MongoClient("mongodb+srv://eyasomai:0000@tutoapp.ipta4hq.mongodb.net/test")
 db = client['test']
@@ -15,6 +16,35 @@ url_js = [
     'https://www.simplilearn.com/what-is-dimensionality-reduction-article/tutorials',
     'https://www.simplilearn.com/resources/software-development/tutorials'
 ]
+
+def clean_data(course):
+    # Mettre en place des valeurs par défaut
+    if 'title' not in course:
+        course['title'] = 'Unknown Title'
+    if 'description' not in course:
+        course['description'] = 'No Description Available'
+    if 'imageLink' not in course:
+        course['imageLink'] = 'No Image Available'
+    if 'link' not in course:
+        course['link'] = 'No Link Available'
+    
+    # Nettoyage des caractères indésirables
+    course['title'] = ''.join(e for e in course['title'] if e.isalnum() or e.isspace())
+    course['description'] = ''.join(e for e in course['description'] if e.isalnum() or e.isspace())
+    course['imageLink'] = course['imageLink'].strip()  # Suppression des espaces en début et fin de chaîne
+    course['link'] = course['link'].strip()  # Suppression des espaces en début et fin de chaîne
+    
+    # Suppression des doublons
+    unique_words = set()
+    course['title'] = ' '.join(word for word in course['title'].split() if word not in unique_words and not unique_words.add(word))
+    
+    # Formatage des données
+    course['title'] = course['title'].upper()
+    
+    # Nettoyage de la description de tout ce qui n'est pas alphanumérique
+    course['description'] = re.sub(r'[^\w\s]', '', course['description'])
+    
+    return course
 
 def parse(url):
     response = requests.get(url)
@@ -34,8 +64,15 @@ def parse(url):
             'link': technology_href
         }
         
-        result = all_courses.insert_one(course_data)
+        cleaned_course = clean_data(course_data)
+        result = all_courses.insert_one(cleaned_course)
+        
+        print(f"Added Course: {cleaned_course['title']}\nImage Link: {cleaned_course['imageLink']}\nDescription: {cleaned_course['description']}\nLink: {cleaned_course['link']}\n")
+        print("Insertion result:", result)
+
+print("Starting Web Scraping...")
 
 for url in url_js:
-    
     parse(url)
+
+print("Web Scraping completed.")
--- backend/scrapers/udemy_scraper.py
+++ backend/scrapers/udemy_scraper.py
@@ -11,16 +11,45 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 category_cards = soup.find_all('a', class_='top-categories--category-card--15wjg')
 
+def clean_data(course):
+    # Mettre en place des valeurs par défaut
+    if 'title' not in course:
+        course['title'] = 'Unknown Title'
+    if 'link' not in course:
+        course['link'] = 'No Link Available'
+    
+    # Nettoyage des caractères indésirables
+    course['title'] = ''.join(e for e in course['title'] if e.isalnum() or e.isspace())
+    
+    # Suppression des doublons
+    unique_words = set()
+    course['title'] = ' '.join(word for word in course['title'].split() if word not in unique_words and not unique_words.add(word))
+    
+    # Formatage des données
+    course['title'] = course['title'].upper()
+    
+    return course
+
+print("Starting Web Scraping...")
+
 for card in category_cards:
     try:
         title = card.find('span', class_='ud-heading-md').text
         link = card['href']
+        
         course_data = {
             'title': title,
             'link': 'https://www.udemy.com' + link,
         }
-        result = all_courses.insert_one(course_data)
-        print(f"Added Course: {title}\nLink: {link}\n")
+        
+        cleaned_course = clean_data(course_data)
+        
+        result = all_courses.insert_one(cleaned_course)
+        
+        print(f"Added Course: {cleaned_course['title']}\nLink: {cleaned_course['link']}\n")
         print("Insertion result:", result)
     except Exception as e:
         print("Error:", e)
+
+print("Web Scraping completed.")
+
